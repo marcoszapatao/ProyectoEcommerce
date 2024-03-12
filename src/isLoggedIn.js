562d6394e8e9d3ccef092213e9dc2c7286d1233f
Marcos Zapata <marcos.zapataota@gmail.com>
2024-03-12 20:49:56 -0300

    Prueba sobre isLoggeedIn

--- src/isLoggedIn.js
+++ src/isLoggedIn.js
@@ -3,7 +3,7 @@ import passport from 'passport';
 function isLoggedIn(req, res, next) {
     passport.authenticate('jwt', { session: false }, (err, user, info) => {
         if (err || !user) {
-
+            console.log("aca el error de isLoggedIn", err)
             return res.redirect('/session/login');
         }
         req.user = user;
